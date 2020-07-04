092509fc5b1a70ebd7d682d17487fb6222f48a30
Papryk <gustaw5022@gmail.com>
2020-07-04 22:27:13 +0200

    added databasehelper

---
+++ SummerRent/app/src/main/java/com/partos/summerrent/db/DataBaseHelper.kt
@@ -0,0 +1,255 @@
+package com.partos.summerrent.db
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+import com.partos.summerrent.models.Date
+import com.partos.summerrent.models.Day
+
+
+object TableInfo : BaseColumns {
+    const val DATABASE_NAME = "Summer Rent"
+    const val TABLE_NAME_SMALL = "Small"
+    const val TABLE_COLUMN_DAY = "day"
+    const val TABLE_COLUMN_MONTH = "month"
+    const val TABLE_COLUMN_YEAR = "year"
+    const val TABLE_COLUMN_DAY_OF_WEEK = "dayOfWeek"
+    const val TABLE_COLUMN_STATUS = "status"
+    const val TABLE_COLUMN_NOTE = "note"
+    const val TABLE_NAME_BIG = "Big"
+
+}
+
+object BasicCommand {
+    const val SQL_CREATE_TABLE_SMALL =
+        "CREATE TABLE ${TableInfo.TABLE_NAME_SMALL} (" +
+                "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+                "${TableInfo.TABLE_COLUMN_DAY} INTEGER NOT NULL," +
+                "${TableInfo.TABLE_COLUMN_MONTH} INTEGER NOT NULL," +
+                "${TableInfo.TABLE_COLUMN_YEAR} INTEGER NOT NULL," +
+                "${TableInfo.TABLE_COLUMN_DAY_OF_WEEK} INTEGER NOT NULL," +
+                "${TableInfo.TABLE_COLUMN_STATUS} INTEGER NOT NULL," +
+                "${TableInfo.TABLE_COLUMN_NOTE} TEXT NOT NULL)"
+
+    const val SQL_CREATE_TABLE_BIG =
+        "CREATE TABLE ${TableInfo.TABLE_NAME_BIG} (" +
+                "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+                "${TableInfo.TABLE_COLUMN_DAY} INTEGER NOT NULL," +
+                "${TableInfo.TABLE_COLUMN_MONTH} INTEGER NOT NULL," +
+                "${TableInfo.TABLE_COLUMN_YEAR} INTEGER NOT NULL," +
+                "${TableInfo.TABLE_COLUMN_DAY_OF_WEEK} INTEGER NOT NULL," +
+                "${TableInfo.TABLE_COLUMN_STATUS} INTEGER NOT NULL," +
+                "${TableInfo.TABLE_COLUMN_NOTE} TEXT NOT NULL)"
+
+    const val SQL_DELETE_TABLE_SMALL = "DROP TABLE IF EXISTS ${TableInfo.TABLE_NAME_SMALL}"
+    const val SQL_DELETE_TABLE_BIG = "DROP TABLE IF EXISTS ${TableInfo.TABLE_NAME_BIG}"
+}
+
+class DataBaseHelper(context: Context) :
+    SQLiteOpenHelper(context, TableInfo.DATABASE_NAME, null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(BasicCommand.SQL_CREATE_TABLE_SMALL)
+        db?.execSQL(BasicCommand.SQL_CREATE_TABLE_BIG)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL(BasicCommand.SQL_CREATE_TABLE_SMALL)
+        db?.execSQL(BasicCommand.SQL_CREATE_TABLE_BIG)
+
+        onCreate(db)
+    }
+
+    fun createDays() {
+
+    }
+
+
+    fun getSmallList(): ArrayList<Day> {
+        var smallList = ArrayList<Day>()
+        val db = readableDatabase
+        val selectQuery = "Select * from ${TableInfo.TABLE_NAME_SMALL}"
+        val result = db.rawQuery(selectQuery, null)
+        if (result.moveToFirst()) {
+            do {
+                var day = Day(
+                    result.getInt(result.getColumnIndex(BaseColumns._ID)).toLong(),
+                    Date(
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_DAY)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_MONTH)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_YEAR)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_DAY_OF_WEEK))
+                    ),
+                    result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_STATUS)),
+                    result.getString(result.getColumnIndex(TableInfo.TABLE_COLUMN_NOTE))
+                )
+                smallList.add(day)
+            } while (result.moveToNext())
+        }
+        result.close()
+        db.close()
+        return smallList
+    }
+
+    fun getBigList(): ArrayList<Day> {
+        var bigList = ArrayList<Day>()
+        val db = readableDatabase
+        val selectQuery = "Select * from ${TableInfo.TABLE_NAME_BIG}"
+        val result = db.rawQuery(selectQuery, null)
+        if (result.moveToFirst()) {
+            do {
+                var day = Day(
+                    result.getInt(result.getColumnIndex(BaseColumns._ID)).toLong(),
+                    Date(
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_DAY)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_MONTH)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_YEAR)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_DAY_OF_WEEK))
+                    ),
+                    result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_STATUS)),
+                    result.getString(result.getColumnIndex(TableInfo.TABLE_COLUMN_NOTE))
+                )
+                bigList.add(day)
+            } while (result.moveToNext())
+        }
+        result.close()
+        db.close()
+        return bigList
+    }
+
+    fun getSmall(day: Int, month: Int, year: Int): Day {
+        var smallList = ArrayList<Day>()
+        val db = readableDatabase
+        val selectQuery =
+            "Select * from ${TableInfo.TABLE_NAME_SMALL} where ${TableInfo.TABLE_COLUMN_DAY} = " +
+                    day.toString() + " and ${TableInfo.TABLE_COLUMN_MONTH} = " + month.toString() +
+                    " and ${TableInfo.TABLE_COLUMN_YEAR} = " + year.toString()
+        val result = db.rawQuery(selectQuery, null)
+        if (result.moveToFirst()) {
+            do {
+                var smallDay = Day(
+                    result.getInt(result.getColumnIndex(BaseColumns._ID)).toLong(),
+                    Date(
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_DAY)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_MONTH)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_YEAR)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_DAY_OF_WEEK))
+                    ),
+                    result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_STATUS)),
+                    result.getString(result.getColumnIndex(TableInfo.TABLE_COLUMN_NOTE))
+                )
+                smallList.add(smallDay)
+            } while (result.moveToNext())
+        }
+
+        result.close()
+        db.close()
+        return smallList[0]
+    }
+
+    fun getBig(day: Int, month: Int, year: Int): Day {
+        var bigList = ArrayList<Day>()
+        val db = readableDatabase
+        val selectQuery =
+            "Select * from ${TableInfo.TABLE_NAME_BIG} where ${TableInfo.TABLE_COLUMN_DAY} = " +
+                    day.toString() + " and ${TableInfo.TABLE_COLUMN_MONTH} = " + month.toString() +
+                    " and ${TableInfo.TABLE_COLUMN_YEAR} = " + year.toString()
+        val result = db.rawQuery(selectQuery, null)
+        if (result.moveToFirst()) {
+            do {
+                var smallDay = Day(
+                    result.getInt(result.getColumnIndex(BaseColumns._ID)).toLong(),
+                    Date(
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_DAY)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_MONTH)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_YEAR)),
+                        result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_DAY_OF_WEEK))
+                    ),
+                    result.getInt(result.getColumnIndex(TableInfo.TABLE_COLUMN_STATUS)),
+                    result.getString(result.getColumnIndex(TableInfo.TABLE_COLUMN_NOTE))
+                )
+                bigList.add(smallDay)
+            } while (result.moveToNext())
+        }
+
+        result.close()
+        db.close()
+        return bigList[0]
+    }
+
+    private fun addSmall(
+        day: Int,
+        month: Int,
+        year: Int,
+        dayOfWeek: Int,
+        status: Int,
+        note: String
+    ): Boolean {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(TableInfo.TABLE_COLUMN_DAY, day)
+        values.put(TableInfo.TABLE_COLUMN_MONTH, month)
+        values.put(TableInfo.TABLE_COLUMN_YEAR, year)
+        values.put(TableInfo.TABLE_COLUMN_DAY_OF_WEEK, dayOfWeek)
+        values.put(TableInfo.TABLE_COLUMN_STATUS, status)
+        values.put(TableInfo.TABLE_COLUMN_NOTE, note)
+        val success = db.insert(TableInfo.TABLE_NAME_SMALL, null, values)
+        db.close()
+        return (Integer.parseInt("$success") != -1)
+    }
+
+    private fun addBig(
+        day: Int,
+        month: Int,
+        year: Int,
+        dayOfWeek: Int,
+        status: Int,
+        note: String
+    ): Boolean {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(TableInfo.TABLE_COLUMN_DAY, day)
+        values.put(TableInfo.TABLE_COLUMN_MONTH, month)
+        values.put(TableInfo.TABLE_COLUMN_YEAR, year)
+        values.put(TableInfo.TABLE_COLUMN_DAY_OF_WEEK, dayOfWeek)
+        values.put(TableInfo.TABLE_COLUMN_STATUS, status)
+        values.put(TableInfo.TABLE_COLUMN_NOTE, note)
+        val success = db.insert(TableInfo.TABLE_NAME_BIG, null, values)
+        db.close()
+        return (Integer.parseInt("$success") != -1)
+    }
+
+    fun updateSmall(day: Day): Boolean {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(TableInfo.TABLE_COLUMN_DAY, day.date.day)
+        values.put(TableInfo.TABLE_COLUMN_MONTH, day.date.month)
+        values.put(TableInfo.TABLE_COLUMN_YEAR, day.date.year)
+        values.put(TableInfo.TABLE_COLUMN_DAY_OF_WEEK, day.date.dayOfWeek)
+        values.put(TableInfo.TABLE_COLUMN_STATUS, day.status)
+        values.put(TableInfo.TABLE_COLUMN_NOTE, day.note)
+        val success = db.update(
+            TableInfo.TABLE_NAME_SMALL, values, BaseColumns._ID + "=?",
+            arrayOf(day.id.toString())
+        ).toLong()
+        return Integer.parseInt("$success") != -1
+    }
+
+    fun updateBig(day: Day): Boolean {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(TableInfo.TABLE_COLUMN_DAY, day.date.day)
+        values.put(TableInfo.TABLE_COLUMN_MONTH, day.date.month)
+        values.put(TableInfo.TABLE_COLUMN_YEAR, day.date.year)
+        values.put(TableInfo.TABLE_COLUMN_DAY_OF_WEEK, day.date.dayOfWeek)
+        values.put(TableInfo.TABLE_COLUMN_STATUS, day.status)
+        values.put(TableInfo.TABLE_COLUMN_NOTE, day.note)
+        val success = db.update(
+            TableInfo.TABLE_NAME_BIG, values, BaseColumns._ID + "=?",
+            arrayOf(day.id.toString())
+        ).toLong()
+        return Integer.parseInt("$success") != -1
+    }
+
+}
